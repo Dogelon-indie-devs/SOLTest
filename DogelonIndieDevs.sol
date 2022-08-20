28bb6f96fce61816fb2018b17bbc135f597d7c93
AX302 <ax302.me@gmail.com>
2022-08-20 13:43:35 +0100

    Simple Get String Call

--- DogelonIndieDevs.sol
+++ DogelonIndieDevs.sol
@@ -1,2 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.16 <0.9.0;
+
+contract DogelonIndieDevsContract {
+
+ string DogelonIndieDevs;
+
+    constructor () {
+      DogelonIndieDevs = "Dogelon Indie Devs @ 2022";
+    }
+
+    function GetDevsData() public view returns (string memory) {
+        return DogelonIndieDevs;
+    }
+
+}
